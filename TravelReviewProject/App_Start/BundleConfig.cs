1f7771d6bb5857251ae10d87b84d20b787afb6a9
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2017-10-20 20:07:23 -0400

    updated on third day

--- TravelReviewProject/App_Start/BundleConfig.cs
+++ TravelReviewProject/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace TravelReviewProject
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-cerulean.css",
+                      "~/Content/bootstrap-cyborg.css",
                       "~/Content/site.css"));
         }
     }
