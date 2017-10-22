7ebd2f535f1b77d5156f25efaa213fa9e837befe
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2017-10-21 20:09:46 -0400

    started work on style

--- TravelReviewProject/Models/Review.cs
+++ TravelReviewProject/Models/Review.cs
@@ -14,11 +14,12 @@ namespace TravelReviewProject.Models
         public int ID { get; set; }
         [DisplayName("Title of Review")]
         public string ReviewTitle { get; set; }
-        [DisplayName("User Rating")]
+        [DisplayName("User Review")]
         public string UserReview { get; set; }
         //how can you make that date to be automatically today?
         /*[DefaultValue(typeof(DateTime), DateTime.Now.ToString("yyyy-MM-dd")),
             DisplayName("Date of Visit")]*/
+        /*is it DateTime.Now.Year, like in the footer?*/
         [DataType(DataType.Date), DisplayName("Date of Visit")]
         public DateTime Date { get; set; }
 
@@ -27,12 +28,12 @@ namespace TravelReviewProject.Models
         [DisplayName("Destination")]
         public virtual Destination Destination { get; set; }
 
-        [ForeignKey("Rating")]
+        [ForeignKey("Rating"), DisplayName("Rating")]
         public int RatingID { get; set; }
         [DisplayName("Rating")]
         public virtual Rating Rating { get; set; }
 
-        [ForeignKey("Cost")]
+        [ForeignKey("Cost"), DisplayName("Estimated Cost")]
         public int CostID { get; set; }
         [DisplayName("Estimated Cost")]
         public virtual Cost Cost { get; set; }
